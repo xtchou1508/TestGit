b9ad70e4f0a5a570eff40d0232582a7c56228588
tom_glb <goodMorning_pro@atguigu.com>
2019-03-11 10:32:43 +0800

    55555

--- src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
+++ src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -4,11 +4,7 @@ public class EurekaServer7001_App {
 
 	public static void main(String[] args) {
 			
-<<<<<<< HEAD
 		System.out.println("TestGit1");
-=======
-		System.out.println("TestGit");
->>>>>>> branch 'master' of https://github.com/xtchou1508/TestGit.git
 		
 	}
 }
